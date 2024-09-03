f27024d5a620d24294b41e2596cccff72f3164ba
Tamnemng <nguyndantdm6@gmail.com>
2024-09-04 00:41:39 +0700

    add login page and change the app childs component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,13 +3,22 @@ import { NgModule } from '@angular/core';
 import { WelcomeComponent } from './pages/welcome/welcome.component';
 import { MonitorComponent } from './pages/monitor/monitor.component';
 import { NoPageComponent } from './pages/no-page/no-page.component';
+import { LoginComponent } from './pages/login/login.component';
+import { AppComponent } from './app.component';
 
 export const routes: Routes = [
-    { path: '', redirectTo: '/welcome', pathMatch: 'full' },
-    { path: 'welcome', component: WelcomeComponent },
-    { path: 'monitor', component: MonitorComponent},
-    { path: '**', component: NoPageComponent},
-];
+    {
+      path: '',
+      component: AppComponent,
+      children: [
+        { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+        { path: 'welcome', component: WelcomeComponent },
+        { path: 'monitor', component: MonitorComponent },
+        { path: '**', component: NoPageComponent },
+      ]
+    },
+    { path: 'login', component: LoginComponent },
+  ];
 
 @NgModule({
     imports: [ RouterModule.forChild(routes) ],
