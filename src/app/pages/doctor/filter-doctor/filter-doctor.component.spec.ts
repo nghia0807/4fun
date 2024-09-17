c28b884aba976a967f4bd4c5e60d8400ca8bea00
Tamnemng <nguyndantdm6@gmail.com>
2024-09-17 13:51:42 +0700

    adding tag to doctors

---
+++ src/app/pages/doctor/filter-doctor/filter-doctor.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FilterDoctorComponent } from './filter-doctor.component';
+
+describe('FilterDoctorComponent', () => {
+  let component: FilterDoctorComponent;
+  let fixture: ComponentFixture<FilterDoctorComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [FilterDoctorComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(FilterDoctorComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
